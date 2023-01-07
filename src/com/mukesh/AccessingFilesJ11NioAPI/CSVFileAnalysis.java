86b39fd4f7efd3f31f815f09bbfbee08ba3040e4
Mukesh A <70726140+mukeshakula418@users.noreply.github.com>
2023-01-07 15:55:35 -0600

    Replacing the code block which reads filter and maps with Streams

--- src/com/mukesh/AccessingFilesJ11NioAPI/CSVFileAnalysis.java
+++ src/com/mukesh/AccessingFilesJ11NioAPI/CSVFileAnalysis.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class CSVFileAnalysis {
 
@@ -14,14 +15,18 @@ public class CSVFileAnalysis {
         Function<String, Person> lineToPerson = line -> lineToPerson(line);
 
         try (BufferedReader reader = Files.newBufferedReader(path)){
-            String line = reader.readLine();
-            while (line != null) {
-                if (!line.startsWith("#")){
-                    Person person = lineToPerson.apply(line);
-                    System.out.println("person = "+ person);
-                }
-                line = reader.readLine();
-            }
+
+            Stream<String> lines = reader.lines();
+            lines.filter(line -> !line.startsWith("#")).map(lineToPerson).forEach(System.out::println);
+
+//            String line = reader.readLine();
+//            while (line != null) {
+//                if (!line.startsWith("#")){
+//                    Person person = lineToPerson.apply(line);
+//                    System.out.println("person = "+ person);
+//                }
+//                line = reader.readLine();
+//            }
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
